9cc19d88afed02e749f259cf156c0797fa041e5a
Aljazek <aradovan5@gmail.com>
2022-07-07 11:33:05 +0100

    optimized date

--- client/src/components/Movies/Movie.jsx
+++ client/src/components/Movies/Movie.jsx
@@ -9,13 +9,15 @@ const Movie = ({ movie, date }) => {
   }, [date]);
 
   return (
-    <div className="p-6 bg-white rounded-lg border w-full h-full flex flex-row justify-evenly text-left mb-10 shadow-2xl">
+    <>
+      {movieDates.length > 0 && (
+        <div className="p-6 bg-white rounded-lg border w-full h-full flex flex-row justify-evenly text-left mb-10 shadow-2xl">
       <div>
         <img src={movie.poster.$t}/>
       </div>
       <div className='ml-3 w-[50%]'>
         <h1 className='text-4xl font-bold mb-2 text-gray-700'>{movie.title}</h1>
-        <h3>{movie.original_title}</h3>
+        <h3 className='italic mb-5'>"{movie.original_title}"</h3>
         <h3><span className='font-bold'>Leto:</span> {movie.year}</h3>
         <h3><span className='font-bold'>Trajanje:</span> {movie.duration} minut</h3>
         <h3><span className='font-bold'>Direktor:</span> {movie.director}</h3>
@@ -24,8 +26,7 @@ const Movie = ({ movie, date }) => {
       </div>
       <div>
         <div className='flex flex-col justify-between rounded-xl '>
-        {movieDates.length > 0 ?
-          movieDates.map((show) => (
+        {movieDates.map((show) => (
             <a href={`https://vstopnice.kolosej.si/WebTicketNet/Performance.aspx?oid=${show.id}`}>
             {show.theater !== 'Dvorana Komuna' && (
               <div className='flex flex-row rounded-lg bg-blue-300 mb-3 hover:scale-105 transition-all ease-in cursor-pointer'>
@@ -35,14 +36,12 @@ const Movie = ({ movie, date }) => {
               </div>
             )}
             </a>
-          )):
-          <div className='bg-red-300 rounded-lg'>
-            <h1 className='mx-5 my-2 '>Nema predstave brale</h1>
-          </div>
-        } 
+          ))}
         </div>
       </div>
     </div>
+      )}
+    </>
   )
 }
 
